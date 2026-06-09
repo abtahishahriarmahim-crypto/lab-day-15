7d6b2aa27b961679acd1a243b71558c50451e568
abtahishahriarmahim-crypto <abtahishahriarmahim@gmail.com>
2026-06-10 11:23:14 +1200

    created the employe salary system where we can add employee search employee and watch employe lists

--- Employee_Salary_System.py
+++ Employee_Salary_System.py
@@ -1,1 +1,68 @@
-# hj
+class EmployeeSystem:
+    def __init__(self):
+        self.__employees = []
+
+    def addEmployee(self, name, position, salary):
+        employee = {
+            "name":name,
+            "position":position,
+            "salary":salary
+        }
+
+        self.__employees.append(employee)
+        print(name, "has been added to Employee system")
+
+
+#this method displays all movies in the watchlist
+    def displayEmployeesRecords(self):
+        if len(self.__employees) == 0:
+            print("No Employee found by this name.")
+
+        else:
+            print("\nEmployees' Records:")
+            print("==================")
+
+            for employee in self.__employees:
+                print("Name:",employee["name"])
+                print("Position:",employee["position"])
+                print("Salary:",employee["salary"])
+                print("====================")
+
+
+#this method is for searching movies bby their title
+    def searchEmployee(self, name):
+        for employee in self.__employees:
+            if employee["name"].lower() == name.lower():
+                print("\nEmployee Found in the system!!")
+                print("Name:",employee["name"])
+                print("Position:",employee["position"])
+                print("Salary:",employee["salary"])
+                return
+            
+        print("Employee not found in the system")
+
+
+
+
+    def countEmployee(self):
+        return len(self.__employees)
+    
+
+employe = EmployeeSystem()
+
+number_of_employee = int(input("How many Employee do you wanna add?? Please enter a valid number:"))
+for i in range(number_of_employee):
+    print("\nEnter Employee", i + 1)
+
+    name = input("Enter employee name:")
+    position = input("Enter employee position:")
+    salary = float(input("Enter employee salary :"))
+
+
+    employe.addEmployee(name,position,salary)
+
+employe.displayEmployeesRecords()
+
+searchEmployeeName = input("\nSearch Employee:")
+employe.searchEmployee(searchEmployeeName)
+print("\nTotal Employees:",employe.countEmployee())
